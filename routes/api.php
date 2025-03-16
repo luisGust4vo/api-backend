9b7c1bc566a3c26b3ad18a8a9b6ddd71c0babb6c
Luis Gustavo <75265319+luisGust4vo@users.noreply.github.com>
2025-03-16 00:35:23 -0300

    Feat:Finalizacao da api de user

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,17 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+// Route::get('/user', function (Request $request) {
+//     return $request->user();
+// })->middleware('auth:sanctum');
+
+
+Route::prefix('user')->group(function () {
+    Route::get('/teste', function (Request $request) {
+        return response()->json(['message' => 'API funcionando, mestre Luiz! 🚀']);
+    });
+    Route::post('/register', [AuthController::class, 'register']);
+    Route::post('/login', [AuthController::class, 'login']);
+});
